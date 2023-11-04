b25d85c10041fa1af4938459c85a29397caff52f
Black <99789748+Dragonkung@users.noreply.github.com>
2023-11-04 10:23:41 +0700

    Update Key.lua

--- Key.lua
+++ Key.lua
@@ -1,6 +1,43 @@
+--เสือก
 local keychecked = false 
 
+--checkkey
+
+
 if _G.Key == "FGHI-6JKLM-7NOPQ8" then
+   keychecked = true
+end
+
+if keychecked == true then
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/Dragonkung/Test/main/kuy.txt"))()
+   else
+    game.Players.LocalPlayer:Kick("คีย์ผิด/ไม่มีคีย์")
+end
+
+--Key2
+if _G.Key == "UVW1-8XYZ2-0ABC3" then
+   keychecked = true
+end
+
+if keychecked == true then
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/Dragonkung/Test/main/kuy.txt"))()
+   else
+    game.Players.LocalPlayer:Kick("คีย์ผิด/ไม่มีคีย์")
+end
+
+--Key3
+if _G.Key == "DEFG-4HIJK-5LMNO" then
+   keychecked = true
+end
+
+if keychecked == true then
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/Dragonkung/Test/main/kuy.txt"))()
+   else
+    game.Players.LocalPlayer:Kick("คีย์ผิด/ไม่มีคีย์")
+end
+
+--KeyTest
+if _G.Key == "Test" then
    keychecked = true
 end
 
